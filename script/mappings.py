21446a669a93f335c6a45066aa70610d43f69fc4
oxfa <104021516+oxfa@users.noreply.github.com>
2024-07-14 00:43:24 +0800

    Update mappings.py

--- script/mappings.py
+++ script/mappings.py
@@ -1,7 +1,7 @@
 LIST_RULESET_MAPPING = {
     "full": "DOMAIN",
     "domain": "DOMAIN-SUFFIX",
-    "regexp": "URL-REGEX",
+    "regexp": "DOMAIN-REGEX",
     "ip-cidr": "IP-CIDR",
     "ip-cidr6": "IP-CIDR6",
     "ip-asn": "IP-ASN",
