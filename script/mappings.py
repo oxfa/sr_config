88031b6733b9f0c497fe0a076495984d3463483f
oxfa <104021516+oxfa@users.noreply.github.com>
2024-07-21 22:00:09 +0800

    Update mappings.py

--- script/mappings.py
+++ script/mappings.py
@@ -1,22 +1,22 @@
 
 LIST_SR_RULESET_MAPPING = {
+    "keyword": "DOMAIN-KEYWORD",
     "full": "DOMAIN",
     "domain": "DOMAIN-SUFFIX",
     "regexp": "URL-REGEX",
     "ip-cidr": "IP-CIDR",
     "ip-cidr6": "IP-CIDR",
-    "ip-asn": "IP-ASN",
-    "keyword": "DOMAIN-KEYWORD"
+    "ip-asn": "IP-ASN"
 }
 
 LIST_CLASH_RULESET_MAPPING = {
+    "keyword": "DOMAIN-KEYWORD",
     "full": "DOMAIN",
     "domain": "DOMAIN-SUFFIX",
     "regexp": "DOMAIN-REGEX",
     "ip-cidr": "IP-CIDR",
     "ip-cidr6": "IP-CIDR6",
-    "ip-asn": "IP-ASN",
-    "keyword": "DOMAIN-KEYWORD"
+    "ip-asn": "IP-ASN"
 }
 
 SR_RULESET_LIST_MAPPING = {v: k for k, v in LIST_SR_RULESET_MAPPING.items()}
